9eb362b6868d471d26a4486dc8d8e9b1738c6a45
Han Kang <hankang@people.cn>
2020-09-25 10:07:01 +0800

    Init Version

---
+++ iOSWidgetSample/iOSWidgetSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSWidgetSample
+//
+//  Created by KANG HAN on 2020/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
